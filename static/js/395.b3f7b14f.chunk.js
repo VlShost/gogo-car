58469056b5e193a5135d44754e29f8271dace5ba
VlShost <113975751+VlShost@users.noreply.github.com>
2023-09-20 16:25:43 +0000

    Deploying to gh-pages from @ VlShost/gogo-car@a196e5c628600dd621158ff7c08fcac46f23da0d 🚀

--- static/js/395.b3f7b14f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgogo_car=self.webpackChunkgogo_car||[]).push([[395],{395:function(n,r,e){e.r(r),e.d(r,{default:function(){return u}});var c=e(184);function u(){return(0,c.jsx)("div",{children:(0,c.jsx)("p",{children:"favorites"})})}}}]);
-//# sourceMappingURL=395.b3f7b14f.chunk.js.map
